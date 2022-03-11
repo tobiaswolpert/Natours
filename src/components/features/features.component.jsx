7ac63adfa49fb287ed9c167614f99b0a854de7bd
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-03-10 22:17:35 -0500

    Finished the features section

--- src/components/features/features.component.jsx
+++ src/components/features/features.component.jsx
@@ -16,6 +16,45 @@ const Features = () => {
             </p>
           </div>
         </div>
+
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <ion-icon name="compass-outline"></ion-icon>
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Meet Nature
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              esse ipsa adipisci dolor eius a et magnam illo.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <ion-icon name="map-outline"></ion-icon>
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Find your way
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              esse ipsa adipisci dolor eius a et magnam illo.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <ion-icon name="heart-outline"></ion-icon>
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Live a healthier life
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              esse ipsa adipisci dolor eius a et magnam illo.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,23 +3,21 @@ import logo from "../../img/logo-white.png";
 
 const Header = () => {
   return (
-    <div>
-      <header className="header">
-        <div className="header__logo-box">
-          <img src={logo} alt="White logo" className="header__logo"></img>
-        </div>
+    <header className="header">
+      <div className="header__logo-box">
+        <img src={logo} alt="White logo" className="header__logo"></img>
+      </div>
 
-        <div className="header__text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary--main">Outdoors</span>
-            <span className="heading-primary--sub">is where life happens</span>
-          </h1>
-          <a href="#" className="btn btn--white btn--animated">
-            Discover our tours
-          </a>
-        </div>
-      </header>
-    </div>
+      <div className="header__text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
+        </h1>
+        <a href="#" className="btn btn--white btn--animated">
+          Discover our tours
+        </a>
+      </div>
+    </header>
   );
 };
 
